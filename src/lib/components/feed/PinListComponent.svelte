b16932c6622a26414549473219ab6c287303098b
ArunaTebel <arunatebel@hotmail.com>
2023-04-09 14:25:57 +0530

    Initial version of the Pins list

---
+++ src/lib/components/feed/PinListComponent.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {onMount} from "svelte";
+    import {supabaseClient} from "$lib/supabaseClient";
+
+    let pins = [];
+
+    onMount(async () => {
+        let {data, error} = await supabaseClient
+            .from('pins')
+            .select('title,url');
+        pins = data
+    });
+</script>
+
+<h1>Pin List</h1>
+<ul>
+    {#each pins as pin}
+        <li>{pin.title}: {pin.url}</li>
+    {/each}
+</ul>
--- src/routes/app/feed/+page.svelte
+++ src/routes/app/feed/+page.svelte
@@ -1,10 +1,11 @@
 <script>
     import SubmitPinFormComponent from "$lib/components/feed/SubmitPinFormComponent.svelte";
+    import PinListComponent from "$lib/components/feed/PinListComponent.svelte";
 </script>
 
 <div class="container p-2 mx-auto">
     <div class="flex flex-col">
         <SubmitPinFormComponent/>
-        <div class="">// Main Content //</div>
+        <PinListComponent/>
     </div>
 </div>
