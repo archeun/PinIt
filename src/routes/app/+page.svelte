52bfe47dd672fb273f9dae03d4899821e9d41a1f
ArunaTebel <arunatebel@hotmail.com>
2023-04-23 21:43:11 +0530

    Improvements to the landing and the home page

--- src/routes/app/+page.svelte
+++ src/routes/app/+page.svelte
@@ -1,5 +1,24 @@
-<div class="container p-2 mx-auto">
-    <div class="flex flex-col">
-        <div class="">App Home Page</div>
+<div class="hero min-h-screen bg-base-100">
+    <div class="hero-content text-center">
+        <div class="max-w-lg">
+            <h1 class="text-8xl font-bold text-accent">ARCHEUN</h1>
+            <p class="py-6">Hello, what are you planning to get done today? Please chose an app from the below to start
+                working ...</p>
+            <div class="grid grid-cols-1">
+                <div class="justify-self-center">
+                    <div class="card w-96 bg-base-300 shadow-xl">
+                        <div class="card-body ">
+                            <h2 class="card-title mx-auto">PinIt</h2>
+                            <p>With this app, you can easily save and organize links to articles, videos, and websites,
+                                and share them with your network through a simple and intuitive interface.</p>
+                            <div class="card-actions justify-center mt-5">
+                                <a href="/app/feed" class="btn btn-primary">Go to app</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h1 class="mt-5 font-bold">More apps coming soon! :)</h1>
+        </div>
     </div>
 </div>
